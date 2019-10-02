7adec7be318d69089a21018f353e059f206863b5
ziedbf <zied.boufaden@mozn.sa>
2019-10-02 12:58:48 +0300

    fix enviroment variables

--- manage.py
+++ manage.py
@@ -7,7 +7,7 @@ from app import blueprint
 from app.main import create_app, db
 from app.main.model import user, blacklist
 
-app = create_app(os.getenv('FLASK_ENV') or 'dev')
+app = create_app(os.getenv('FLASK_ENV') or 'developement')
 app.register_blueprint(blueprint)
 
 app.app_context().push()
