97dffd53d5f3bfb25c040cf2cc7e7bb4a68bcad1
ziedbf <zied.boufaden@mozn.sa>
2019-10-02 13:19:04 +0300

    trigger cicd pipeline

--- manage.py
+++ manage.py
@@ -7,7 +7,7 @@ from app import blueprint
 from app.main import create_app, db
 from app.main.model import user, blacklist
 
-app = create_app(os.getenv('FLASK_ENV') or 'developement')
+app = create_app(os.getenv('FLASK_ENV') or 'development')
 app.register_blueprint(blueprint)
 
 app.app_context().push()
